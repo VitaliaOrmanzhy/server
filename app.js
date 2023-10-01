ff0747510130270baca1884da3b7c91e562ce283
VitaliaOrmanzhy <vormanzi812@gmail.com>
2023-10-01 14:38:17 +0300

    server

---
+++ app.js
@@ -0,0 +1,72 @@
+const http = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
+
+http
+  .createServer(function (request, response) {
+    console.log("request ", request.url);
+
+    let filePath = request.url;
+    if (filePath == "/") {
+      filePath = "/index.html";
+    }
+    filePath = path.join(__dirname, filePath);
+    console.log("filepath ", filePath);
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    console.log("extname ", extname);
+    const mimeTypes = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
+    };
+
+    const contentType = mimeTypes[extname] || "application/octet-stream";
+
+    console.log("contentType ", contentType);
+
+    try {
+    fs.readFile(filePath, function (error, content) {
+        console.log('!!!')
+      if (error) {
+
+        if (error.code == "ENOENT") {
+          fs.readFile("./404.html", function (error, content) {
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.end(content, "utf-8");
+          });
+        } else {
+        console.log('error', error.code, error.statusText);
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n",
+          );
+        }
+    
+      } else {
+        console.log('test');
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(content, "utf-8");
+        
+      }
+    });
+} catch(error) {
+    console.error(error.message);
+}
+  })
+  .listen(8080);
+console.log("Server running at http://127.0.0.1:8080/");
